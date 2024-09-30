ab52109fb1fdba0ef7f76650dbe85d586fd959fa
JRC-Chorus <jrc.chorus@gmail.com>
2024-10-01 01:08:08 +0900

    ✨ Added MemberStatus for accessManager()

--- packages/backend/src/core/research.ts
+++ packages/backend/src/core/research.ts
@@ -4,6 +4,9 @@
  * 基本的にはここの関数を定期実行し，関数内でそれぞれのステータスに応じた処理を実行する
  */
 
+import { MemberStatus } from "backend/schema/app";
+import { MemberID } from "backend/schema/db/member";
+
 export function researchManager() {
   // セッション一覧・設定シートを確認し，開催中の調査（リマインドの送付，終了案内の送付等）や開始すべき調査について確認する
 
@@ -17,4 +20,12 @@ export function researchManager() {
 
 
   // 完全に終了した後の処理（不要になったデータの削除など？）
+}
+
+/**
+ * フロントエンドからのアクセスがあったときに，当該アクセスに対するレスポンスを定義
+ */
+export function accessManager(memberId: MemberID): MemberStatus {
+  // TODO: メンバーIDを参照して適切なステータスを返す関数を実装
+  return { status: 'noAns' }
 }
--- packages/backend/src/schema/app.ts
+++ packages/backend/src/schema/app.ts
@@ -2,6 +2,7 @@
  * フロントエンドとの通信に関連のある型定義をおく
  */
 import { z } from 'zod';
+import { Answer } from './db/answer';
 import { RvDate } from './db/common';
 
 export const UrlParams = z.object({
@@ -11,3 +12,22 @@ export const UrlParams = z.object({
   endDate: RvDate,
 });
 export type UrlParams = z.infer<typeof UrlParams>;
+
+// アクセスしたメンバーのステータスを返す
+// 当該セッションは終了済み
+const MSFinished = z.object({
+  status: z.enum(['finished']),
+});
+type MSFinished = z.infer<typeof MSFinished>;
+// 当該セッションに回答済み（回答の変更が可能な期間）
+const MSAlreadyAns = z.object({
+  status: z.enum(['alreadyAns']),
+  ansData: Answer.array(),
+});
+type MSAlreadyAns = z.infer<typeof MSAlreadyAns>;
+// 当該セッションに未回答
+const MSNoAns = z.object({
+  status: z.enum(['noAns']),
+});
+type MSNoAns = z.infer<typeof MSNoAns>;
+export type MemberStatus = MSNoAns | MSAlreadyAns | MSFinished;
