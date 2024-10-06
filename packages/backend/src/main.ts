cb2fff72190d28341c7c265c4ecf4cba291a7a98
JRC-Chorus <jrc.chorus@gmail.com>
2024-10-07 00:39:04 +0900

    ✨ Added accessManager in API list

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,3 +1,4 @@
+// import { accessManager } from './core/access';
 import { migrateEnv } from './core/migrate';
 import { constructHomePage } from './core/page';
 import { researchManager } from './core/research';
@@ -6,10 +7,13 @@ import { getSampleData } from './source/spreadsheet/sample';
 
 declare const global: FrontAPI;
 
-/** APIをGASに登録 */
+/** システムが動作するために実行不可欠な関数 */
 global.doGet = constructHomePage;
 global.migrateEnv = migrateEnv;
 global.researchManager = researchManager;
+
+/** フロントエンドに向けたAPI */
+// global.accessManager = accessManager;
 
 /** サンプルAPI（本番時には元の関数を含めて削除） */
 global.getSampleData = getSampleData;
