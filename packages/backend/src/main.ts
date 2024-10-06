a6968373b26595f74799fa0c7a3658bd18cca937
JRC-Chorus <jrc.chorus@gmail.com>
2024-10-07 00:29:18 +0900

    🔥 Removed obsolete API

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -3,14 +3,12 @@ import { constructHomePage } from './core/page';
 import { researchManager } from './core/research';
 import { FrontAPI } from './schema/api';
 import { getSampleData } from './source/spreadsheet/sample';
-import { getUrlParams } from './source/urlParam';
 
 declare const global: FrontAPI;
 
 /** APIをGASに登録 */
 global.doGet = constructHomePage;
 global.migrateEnv = migrateEnv;
-global.getParams = getUrlParams;
 global.researchManager = researchManager;
 
 /** サンプルAPI（本番時には元の関数を含めて削除） */
