165f451fca285303dbb0b4e11de664df7a02ffd7
JRC-Chorus <jrc.chorus@gmail.com>
2024-10-27 15:28:35 +0900

    ✨ Added prevent reload dialog

--- packages/frontend/src/App.vue
+++ packages/frontend/src/App.vue
@@ -1,4 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+// 再読み込みの確認ダイアログを表示
+window.onbeforeunload = (e) => {
+  e.preventDefault()
+};
+</script>
 
 <template>
   <router-view />
