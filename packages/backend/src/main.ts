2ce2624dc63e2d80a9310e244a0274512e1f7f26
JRC-Chorus <jrc.chorus@gmail.com>
2024-10-29 23:18:10 +0900

    🚑 Added undefined func

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,5 +1,5 @@
 import { FrontAPI } from '@research-vacant/common';
-import { accessManager } from './core/access';
+import { accessManager, submitAnswers } from './core/access';
 import { migrateEnv } from './core/migrate';
 import { constructHomePage } from './core/page';
 import { researchManager } from './core/research';
@@ -14,6 +14,7 @@ global.researchManager = researchManager;
 
 /** フロントエンドに向けたAPI */
 global.accessManager = accessManager;
+global.submitAnswers = submitAnswers;
 
 /** サンプルAPI（本番時には元の関数を含めて削除） */
 global.getSampleData = getSampleData;
