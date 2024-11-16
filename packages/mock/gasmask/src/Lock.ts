49fe49abe591e8fd3df3f0ad3f7b95dc8bef8367
JRC-Chorus <jrc.chorus@gmail.com>
2024-11-16 20:50:28 +0900

    💩 Removed lock process in mock and Added simplified codes

--- packages/mock/gasmask/src/Lock.ts
+++ packages/mock/gasmask/src/Lock.ts
@@ -33,84 +33,9 @@ class Lock implements GoogleAppsScript.Lock.Lock {
     this.isLock = false;
   }
   tryLock(timeoutInMillis: GoogleAppsScript.Integer): boolean {
-    let isSuccess = false;
-    const interValId = setInterval(() => {
-      if (!this.isLock) {
-        this.isLock = true;
-        isSuccess = true;
-        clearInterval(interValId);
-      }
-    }, 100);
-
-    setTimeout(() => clearInterval(interValId), timeoutInMillis);
-    return isSuccess;
+    return true;
   }
   waitLock(timeoutInMillis: GoogleAppsScript.Integer): void {
-    const isSuccess = this.tryLock(timeoutInMillis);
-    if (!isSuccess) {
-      throw new Error('Failed to get a lock');
-    }
+    throw new Error('Method not implemented.');
   }
-}
-
-/** In Source Testing */
-if (import.meta.vitest) {
-  const { test, expect } = import.meta.vitest;
-  global.LockService = new LockServiceClass();
-
-  let baseData = '';
-
-  /**
-   *  データベースを更新するサンプル関数
-   * （更新に５秒かかる想定）
-   */
-  function dummySetDataFunc(setVal: string) {
-    return new Promise<void>((resolve) => {
-      let tmpData = baseData;
-      setTimeout(() => {
-        tmpData += ` ${setVal}`;
-        baseData = tmpData;
-        resolve();
-      }, 5000);
-    });
-  }
-
-  test.skip('no lock', async () => {
-    // 条件を揃えるために元データをリセット
-    baseData = 'FIRST DATA';
-
-    // データを複数回更新（同時アクセスのサンプル）
-    await Promise.all([
-      dummySetDataFunc('MIDDLE DATA'),
-      dummySetDataFunc('FINAL DATA'),
-    ]);
-
-    // middle dataが先祖返りしている
-    expect(baseData).toBe(['FIRST DATA', 'FINAL DATA'].join(' '));
-  });
-
-  test('use lock', async () => {
-    // 条件を揃えるために元データをリセット
-    baseData = 'FIRST DATA';
-
-    // lockを使って処理を実行
-    const warpLock = async (func: () => Promise<void>) => {
-      const lock = LockService.getScriptLock();
-      if (lock.tryLock(10 * 1000)) {
-        await func();
-      }
-      lock.releaseLock();
-    };
-
-    // データを複数回更新（同時アクセスのサンプル）
-    await Promise.all([
-      warpLock(() => dummySetDataFunc('MIDDLE DATA')),
-      warpLock(() => dummySetDataFunc('FINAL DATA')),
-    ]);
-
-    // middle dataを含めてすべてのデータが格納されている
-    expect(baseData).toBe(
-      ['FIRST DATA', 'MIDDLE DATA', 'FINAL DATA'].join(' ')
-    );
-  });
 }
