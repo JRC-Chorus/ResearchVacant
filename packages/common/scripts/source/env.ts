981941ba46d851ffe5e268e0f2eb770332bbebf6
JRC-Chorus <jrc.chorus@gmail.com>
2025-01-03 19:31:33 +0900

    ✨ Added `updateEnv()`

--- packages/common/scripts/source/env.ts
+++ packages/common/scripts/source/env.ts
@@ -1,4 +1,5 @@
 import fs from 'fs';
+import { toEntries } from '../obj/obj';
 
 export class EnvHandler {
   static loadEnv(path: string) {
@@ -13,6 +14,21 @@ export class EnvHandler {
     return env;
   }
 
+  /**
+   * 既存のEnvがある場合には，引数で与えられた値のみを更新して保存する
+   */
+  static updateEnv(path: string, env: Record<string, string>) {
+    const existedEnv = this.loadEnv(path);
+    toEntries(env).map(([k, v]) => {
+      existedEnv[k] = v;
+    });
+
+    this.writeEnv(path, existedEnv);
+  }
+
+  /**
+   * 既存のEnvを上書きするように与えられた値通りのファイルを保存する
+   */
   static writeEnv(path: string, env: Record<string, string>) {
     fs.writeFileSync(
       path,
