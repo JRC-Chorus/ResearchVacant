b9b743591d131f17d6266ec231e05afc9ef1082c
JRC-Chorus <jrc.chorus@gmail.com>
2025-01-07 23:26:33 +0900

    🏷️ Fixed the type definision error in FrontAPI

--- packages/proxy/src/api.ts
+++ packages/proxy/src/api.ts
@@ -1,14 +1,43 @@
-import { zValidator } from '@hono/zod-validator';
 import { FrontAPIkeys } from '@research-vacant/common';
 import { Hono } from 'hono';
 import { handle } from 'hono/aws-lambda';
-import { z } from 'zod';
+import { validator } from 'hono/validator';
 
 const app = new Hono();
 
+// TODO: FrontAPIの定義がZodに戻った場合はValidatorを下記に戻す
+// zValidator('param', z.object({ func: FrontAPIkeys })),
+
 app.get(
   '/:func',
-  zValidator('param', z.object({ func: FrontAPIkeys })),
+  validator('param', (value, c) => {
+    const funcName = value['func'];
+    if (!FrontAPIkeys.some((fName) => fName === funcName)) {
+      return c.json(
+        {
+          success: false,
+          error: {
+            issues: [
+              {
+                received: funcName,
+                code: 'invalid_enum_value',
+                options: FrontAPIkeys,
+                path: ['func'],
+                message: `Invalid enum value. Expected ${FrontAPIkeys.map(
+                  (n) => `'${n}'`
+                ).join(' | ')}, received '${funcName}'`,
+              },
+            ],
+            name: 'ZodError_Imitated',
+          },
+        },
+        400
+      );
+    }
+    return {
+      func: funcName,
+    };
+  }),
   async (c) => {
     const { func } = c.req.param();
     const queryParams = new URLSearchParams(c.req.query());
