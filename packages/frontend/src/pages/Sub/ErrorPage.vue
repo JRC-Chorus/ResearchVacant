247c4c0b372a86b09771135ee695a66f0fe15eea
JRC-Chorus <jrc.chorus@gmail.com>
2025-01-13 23:31:26 +0900

    `fetch()`のエラーキャッチ (#23)
    * 🩹 Fixed a showing texts in the error page
    
    * 🥅 Catch an error at `fetch()`
    
    * 🚨 Apply Prettier Change
    
    ---------
    
    Co-authored-by: JRC-Chorus <JRC-Chorus@users.noreply.github.com>

--- packages/frontend/src/pages/Sub/ErrorPage.vue
+++ packages/frontend/src/pages/Sub/ErrorPage.vue
@@ -20,11 +20,13 @@ const mainStore = useMainStore();
         </q-card-section>
         <q-card-section class="q-pa-none">
           <q-card-section>
-            <div class="error-title">{{ mainStore.error?.name ?? title }}</div>
+            <div class="error-title">
+              {{ mainStore.error?.message ?? title }}
+            </div>
           </q-card-section>
           <q-separator inset class="q-my-md" />
           <q-card-section style="white-space: pre-line">
-            {{ mainStore.error?.message ?? message?.join('\n') }}
+            {{ mainStore.error?.cause ?? message?.join('\n') }}
           </q-card-section>
         </q-card-section>
       </q-card-section>
