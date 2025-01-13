dc7741fc0bd601d3e13f17fcff89da7be923f163
JRC-Chorus <JRC-Chorus@users.noreply.github.com>
2025-01-13 06:31:30 +0000

    🚨 Apply Prettier Change

--- packages/frontend/src/pages/Sub/LoadingPage.vue
+++ packages/frontend/src/pages/Sub/LoadingPage.vue
@@ -20,18 +20,19 @@
   border: 4px solid;
   box-sizing: border-box;
   border-radius: 50%;
-  background: 
-    radial-gradient(circle 5px, currentColor 95%,#0000),
-    linear-gradient(currentColor 50%,#0000 0) 50%/4px 60% no-repeat;
+  background: radial-gradient(circle 5px, currentColor 95%, #0000),
+    linear-gradient(currentColor 50%, #0000 0) 50%/4px 60% no-repeat;
   animation: l1 2s infinite linear;
 }
 .loader:before {
-  content: "";
+  content: '';
   flex: 1;
-  background:linear-gradient(currentColor 50%,#0000 0) 50%/4px 80% no-repeat;
+  background: linear-gradient(currentColor 50%, #0000 0) 50%/4px 80% no-repeat;
   animation: inherit;
 }
 @keyframes l1 {
-  100% {transform: rotate(1turn)}
+  100% {
+    transform: rotate(1turn);
+  }
 }
 </style>
